36344088e0b8cea406d871a14afccfb0535789ed
Hao Dian Li <hli144@stevens.edu>
2022-09-25 00:30:28 -0400

    prints indi table, but only id, name, and gender

--- sourcecode.py
+++ sourcecode.py
@@ -27,19 +27,19 @@ red = "\033[0;91m"
 white = "\033[0;37m"
 
 f = open(arg_1, 'r')
-# print ("Analyzing: " + str(sys.argv))
+print ("Analyzing: " + str(sys.argv))
 
 x = PrettyTable()
 y = PrettyTable()
 x.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"]
 y.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
 
-# def ptables(id, fname, lname, fid): 
-#     x.add_row(id, fname, lname, fid)
+# define variables for table x 
+tbl_id = None; tbl_name = None; tbl_gend = None; tbl_birt = None; tbl_age = None; tbl_aliv = None; tbl_deat = None; tbl_chil = None; tbl_spou = None
 
-print(x)
+# define variables for table y
 
-"""
+# """
 for line in f:
 
     ogline = line
@@ -61,7 +61,7 @@ for line in f:
             if a+1+i > linelen:
                 if line[a+1:i+1] in validtags: #second token is a valid tag
                     if (level == "1") and (line[a+1:i+1] in singletag):
-                        print("<-- " + level + "|" + line[a+1:i+1] + "|" + "Y|")
+                        if line[a+1:i+1] == "NAME": print("<-- " + level + "|" + line[a+1:i+1] + "|Y|")
                     else:
                         #raise ValueError('Invalid level with the tag: <' + line[a+1:i+1] + '>')
                         print("Invalid level: <" + level + "> with the tag: <" + line[a+1:i+1] + ">")
@@ -75,11 +75,14 @@ for line in f:
         if tag in validtags: #second token is a valid tag
 
             if (line[0] == "0") and (tag in tag0):
-                print("<-- " + level + "|" + tag + "|" + "Y|" + line[i+1:])
+                print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
             elif (line[0] == "1") and (tag in tag1):
-                print("<-- " + level + "|" + tag + "|" + "Y|" + line[i+1:])
+                print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
+                if (tag == "NAME"): tbl_name = line[i+1:]
+                if (tag == "SEX"): tbl_gend = line[i+1:]
             elif (line[0] == "2") and (tag in tag2):
-                print("<-- " + level + "|" + tag + "|" + "Y|" + line[i+1:])
+                print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
+                #if (tag == "DEAT"): tbl_alive = "he need some milk"
             else:
                 #raise ValueError('Invalid level with the tag: <' + tag + '>')
                 print("Invalid level: <" + level + "> with the tag: <" + tag + ">.")
@@ -91,7 +94,19 @@ for line in f:
             if tag in validtags:
                 if tag in extag: #INDI, FAM only
                     if (line[0] == "0"):
-                        print("<-- " + line[0] + "|" + tag + "|" + "Y|" + line[2:i+1])
+                        print("<-- " + line[0] + "|" + tag + "|Y|" + line[2:i+1])
+                        if (tag == "INDI"):
+                            # check if we've been here before and write-out then clear previous table data if we have
+                            if (tbl_id != None):
+                                print (red + "Next Customer!" + white)
+                                x.add_row([tbl_id, tbl_name, tbl_gend, tbl_birt, tbl_age, tbl_aliv, tbl_deat, tbl_chil, tbl_spou])
+                                tbl_id = None; tbl_name = None; tbl_gend = None; tbl_birt = None; tbl_age = None; tbl_aliv = None; tbl_deat = None; tbl_chil = None; tbl_spou = None
+                                tbl_id = line[2:i+1]
+                            elif (tbl_id == None):
+                                tbl_id = line[2:i+1]
+                        if (line[2:i+1] == "FAM"):
+                            # reset table y from 0 terminater
+                            tbl_id = None;
                     else:
                         #raise ValueError('Invalid level with the tag: <' + tag + '>')
                         print("Invalid level: <" + level + "> with the tag: <" + tag + ">.")
@@ -109,5 +124,12 @@ for line in f:
             print("--> " + line)
             #raise ValueError('Invalid level: <' + level + '>')
             print("Invalid level: <" + level + ">")
-"""
+# """
+
+# capture the last table entry since there are no more terminaters (0)
+x.add_row([tbl_id, tbl_name, tbl_gend, tbl_birt, tbl_age, tbl_aliv, tbl_deat, tbl_chil, tbl_spou])
+# y.add_row([bla,bla])
+print(x)
+
+
 f.close()
