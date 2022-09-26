8b5ee26f0b9bf8dd7eb7646c58cab9455a994d40
Hao Dian Li <hli144@stevens.edu>
2022-09-26 19:46:09 -0400

    updated FAM table, but husb wife names undone

--- sourcecode.py
+++ sourcecode.py
@@ -1,6 +1,7 @@
 #I pledge my honor that I have abided by the Stevens Honors System.
 #Hao Dian Li
 
+from platform import java_ver
 import sys
 import os #Hack to make colors work in Windows cmd 1/2
 os.system('color') #Hack to make colors work in Windows cmd 2/2
@@ -36,8 +37,9 @@ x.field_names = ["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "C
 y.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"]
 
 # define variables for table x 
-tbl_id = "N/A"; tbl_name = "N/A"; tbl_gend = "N/A"; tbl_birt = "N/A"; tbl_age = "N/A"; tbl_aliv = "N/A"; tbl_deat = "N/A"; tbl_chil = "N/A"; tbl_spou = "N/A"
-birfday = 0; deafday = 0; todays_date = date.today()
+tblx_id = "N/A"; tblx_name = "N/A"; tblx_gend = "N/A"; tblx_birt = "N/A"; tblx_age = "N/A"; tblx_aliv = "N/A"; tblx_deat = "N/A"; tblx_chil = "N/A"; tblx_spou = "N/A"
+tbly_id = "N/A"; tbly_marr = "N/A"; tbly_divo = "N/A"; tbly_husi = "N/A"; tbly_husn = "N/A"; tbly_wifi = "N/A"; tbly_wifn = "N/A"; tbly_chil = "N/A"
+birfday = 0; deafday = 0; todays_date = date.today(); tblx_sarr = []; tblx_carr = []; marfday = 0; divfday = 0; tbly_carr = []
 
 # define variables for table y
 
@@ -80,18 +82,39 @@ for line in f:
                 print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
             elif (line[0] == "1") and (tag in tag1):
                 # print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
-                if (tag == "NAME"): tbl_name = line[i+1:]
-                if (tag == "SEX"): tbl_gend = line[i+1:]
+                if (tag == "NAME"): tblx_name = line[i+1:]
+                if (tag == "SEX"): tblx_gend = line[i+1:]
                 if (tag == "BIRT"): birfday = 1
                 if (tag == "DEAT"): deafday = 1
-                if (tag == "FAMC"): tbl_chil = "'" + line[i+1:] + "'"
-                if (tag == "FAMS"): tbl_spou = "'" + line[i+1:] + "'"
+                if (tag == "FAMC"): tblx_carr.append(line[i+1:]); tblx_chil = tblx_carr
+                if (tag == "FAMS"): tblx_sarr.append(line[i+1:]); tblx_spou = tblx_sarr
+
+                if (tag == "MARR"): marfday = 1
+                if (tag == "DIV"): divfday = 1
+                if (tag == "CHIL"): tbly_carr.append(line[i+1:]); tbly_chil = tbly_carr
+                # if (tag == "HUSB"): 
+                #     tbly_husi = line[i+1:]
+                #     row = x[0]
+                #     col = x[0]
+                #     col = col.get_string(fields = [tbly_husi])
+                #     print(col)
+                #     row.border = False; row.header = False
+                #     name_lookup = row.get_string(fields = ["Name"])
+                #     tbly_husn = name_lookup
+
+                # if (tag == "WIFE"): 
+                #     tbly_wifi = line[i+1:]
+                #     # tbly_wifn = 
+
 
             elif (line[0] == "2") and (tag in tag2):
                 # print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
                 if (tag == "DATE"): 
-                    if (birfday == 1): tbl_birt = line[i+1:]; birfday = 0
-                    if (deafday == 1): tbl_deat = line[i+1:]; deafday = 0; tbl_aliv = False
+                    if (birfday == 1): tblx_birt = line[i+1:]; birfday = 0
+                    if (deafday == 1): tblx_deat = line[i+1:]; deafday = 0; tblx_aliv = False
+
+                    if (marfday == 1): tbly_marr = line[i+1:]; marfday = 0
+                    if (divfday == 1): tbly_divo = line[i+1:]; divfday = 0
             else:
                 #raise ValueError('Invalid level with the tag: <' + tag + '>')
                 print("Invalid level: <" + level + "> with the tag: <" + tag + ">.")
@@ -106,24 +129,31 @@ for line in f:
                         print("<-- " + line[0] + "|" + tag + "|Y|" + line[2:i+1])
                         if (tag == "INDI"):
                             # check if we've been here before and write-out then clear previous table data if we have
-                            if (tbl_id != "N/A"):
+                            if (tblx_id != "N/A"):
                                 # print (red + "Next Customer!" + white)
-                                birth_split = tbl_birt.split(" ")
-                                death_split = tbl_deat.split(" ")
-                                if tbl_aliv == True:
-                                    tbl_age = todays_date.year - int(birth_split[2])
-                                elif tbl_aliv == False:
-                                    tbl_age = int(death_split[2]) - int(birth_split[2])
-                                x.add_row([tbl_id, tbl_name, tbl_gend, tbl_birt, tbl_age, tbl_aliv, tbl_deat, tbl_chil, tbl_spou])
-                                tbl_id = "N/A"; tbl_name = "N/A"; tbl_gend = "N/A"; tbl_birt = "N/A"; tbl_age = "N/A"; tbl_aliv = "N/A"; tbl_deat = "N/A"; tbl_chil = "N/A"; tbl_spou = "N/A"
-                                tbl_id = line[2:i+1]
-                                tbl_aliv = True
-                            elif (tbl_id == "N/A"):
-                                tbl_id = line[2:i+1]
-                                tbl_aliv = True
-                        if (line[2:i+1] == "FAM"):
+
+                                birth_split = tblx_birt.split(" ")
+                                death_split = tblx_deat.split(" ")
+                                if tblx_aliv == True:
+                                    tblx_age = todays_date.year - int(birth_split[2])
+                                elif tblx_aliv == False:
+                                    tblx_age = int(death_split[2]) - int(birth_split[2])
+                                    
+                                x.add_row([tblx_id, tblx_name, tblx_gend, tblx_birt, tblx_age, tblx_aliv, tblx_deat, tblx_chil, tblx_spou])
+                                tblx_id = "N/A"; tblx_name = "N/A"; tblx_gend = "N/A"; tblx_birt = "N/A"; tblx_age = "N/A"; tblx_aliv = "N/A"; tblx_deat = "N/A"; tblx_chil = "N/A"; tblx_spou = "N/A"; tblx_carr = []; tblx_sarr = []
+                                tblx_id = line[2:i+1]
+                                tblx_aliv = True
+                            elif (tblx_id == "N/A"):
+                                tblx_id = line[2:i+1]
+                                tblx_aliv = True
+                        if (tag == "FAM"):
                             # reset table y from 0 terminater
-                            tbl_id = "N/A"
+                            if (tbly_id != "N/A"):
+                                y.add_row([tbly_id, tbly_marr, tbly_divo, tbly_husi, tbly_husn, tbly_wifi, tbly_wifn, tbly_chil])
+                                tbly_id = "N/A"; tbly_marr = "N/A"; tbly_divo = "N/A"; tbly_husi = "N/A"; tbly_husn = "N/A"; tbly_wifi = "N/A"; tbly_wifn = "N/A"; tbly_chil = "N/A"; tbly_carr = []
+                                tbly_id = line[2:i+1]
+                            elif (tbly_id == "N/A"):
+                                tbly_id = line[2:i+1]
                     else:
                         #raise ValueError('Invalid level with the tag: <' + tag + '>')
                         print("Invalid level: <" + level + "> with the tag: <" + tag + ">.")
@@ -143,16 +173,28 @@ for line in f:
             print("Invalid level: <" + level + ">")
 # """
 
+#this is INDI table
 # capture the last table entry since there are no more terminaters (0)
-birth_split = tbl_birt.split(" ")
-death_split = tbl_deat.split(" ")
-if tbl_aliv == True:
-    tbl_age = todays_date.year - int(birth_split[2])
-elif tbl_aliv == False:
-    tbl_age = int(death_split[2]) - int(birth_split[2])
-x.add_row([tbl_id, tbl_name, tbl_gend, tbl_birt, tbl_age, tbl_aliv, tbl_deat, tbl_chil, tbl_spou])
-# y.add_row([bla,bla])
-print(x)
+birth_split = tblx_birt.split(" ")
+death_split = tblx_deat.split(" ")
+if tblx_aliv == True:
+    tblx_age = todays_date.year - int(birth_split[2])
+elif tblx_aliv == False:
+    tblx_age = int(death_split[2]) - int(birth_split[2])
+    
+x.add_row([tblx_id, tblx_name, tblx_gend, tblx_birt, tblx_age, tblx_aliv, tblx_deat, tblx_chil, tblx_spou])
 
+y.add_row([tbly_id, tbly_marr, tbly_divo, tbly_husi, tbly_husn, tbly_wifi, tbly_wifn, tbly_chil])
+
+print("Individuals")
+print(x.get_string(sortby = "ID"))
+print("Families")
+print(y.get_string(sortby = "ID"))
+
+# print(x.get_string(fields=["ID", "Name"]))
+# nlrow = 0
+# while nlrow < 1000:
+#     print(x[nlrow].get_string(fields=["Name"]))
+#     nlrow += 1
 
 f.close()
