d30023c0d2374bea7db1c349cadc1d905c33c944
Hao Dian Li <hli144@stevens.edu>
2022-09-26 17:36:45 -0400

    updated code for age, child, and spouse

--- sourcecode.py
+++ sourcecode.py
@@ -5,6 +5,7 @@ import sys
 import os #Hack to make colors work in Windows cmd 1/2
 os.system('color') #Hack to make colors work in Windows cmd 2/2
 from prettytable import PrettyTable
+from datetime import date
 
 validtags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
 tag0 = ["INDI", "FAM", "HEAD", "TRLR", "NOTE"]
@@ -36,8 +37,7 @@ y.field_names = ["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wif
 
 # define variables for table x 
 tbl_id = "N/A"; tbl_name = "N/A"; tbl_gend = "N/A"; tbl_birt = "N/A"; tbl_age = "N/A"; tbl_aliv = "N/A"; tbl_deat = "N/A"; tbl_chil = "N/A"; tbl_spou = "N/A"
-birfday = 0
-deafday = 0
+birfday = 0; deafday = 0; todays_date = date.today()
 
 # define variables for table y
 
@@ -79,13 +79,16 @@ for line in f:
             if (line[0] == "0") and (tag in tag0):
                 print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
             elif (line[0] == "1") and (tag in tag1):
-                print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
+                # print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
                 if (tag == "NAME"): tbl_name = line[i+1:]
                 if (tag == "SEX"): tbl_gend = line[i+1:]
                 if (tag == "BIRT"): birfday = 1
                 if (tag == "DEAT"): deafday = 1
+                if (tag == "FAMC"): tbl_chil = "'" + line[i+1:] + "'"
+                if (tag == "FAMS"): tbl_spou = "'" + line[i+1:] + "'"
+
             elif (line[0] == "2") and (tag in tag2):
-                print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
+                # print("<-- " + level + "|" + tag + "|Y|" + line[i+1:])
                 if (tag == "DATE"): 
                     if (birfday == 1): tbl_birt = line[i+1:]; birfday = 0
                     if (deafday == 1): tbl_deat = line[i+1:]; deafday = 0; tbl_aliv = False
@@ -104,7 +107,13 @@ for line in f:
                         if (tag == "INDI"):
                             # check if we've been here before and write-out then clear previous table data if we have
                             if (tbl_id != "N/A"):
-                                print (red + "Next Customer!" + white)
+                                # print (red + "Next Customer!" + white)
+                                birth_split = tbl_birt.split(" ")
+                                death_split = tbl_deat.split(" ")
+                                if tbl_aliv == True:
+                                    tbl_age = todays_date.year - int(birth_split[2])
+                                elif tbl_aliv == False:
+                                    tbl_age = int(death_split[2]) - int(birth_split[2])
                                 x.add_row([tbl_id, tbl_name, tbl_gend, tbl_birt, tbl_age, tbl_aliv, tbl_deat, tbl_chil, tbl_spou])
                                 tbl_id = "N/A"; tbl_name = "N/A"; tbl_gend = "N/A"; tbl_birt = "N/A"; tbl_age = "N/A"; tbl_aliv = "N/A"; tbl_deat = "N/A"; tbl_chil = "N/A"; tbl_spou = "N/A"
                                 tbl_id = line[2:i+1]
@@ -135,6 +144,12 @@ for line in f:
 # """
 
 # capture the last table entry since there are no more terminaters (0)
+birth_split = tbl_birt.split(" ")
+death_split = tbl_deat.split(" ")
+if tbl_aliv == True:
+    tbl_age = todays_date.year - int(birth_split[2])
+elif tbl_aliv == False:
+    tbl_age = int(death_split[2]) - int(birth_split[2])
 x.add_row([tbl_id, tbl_name, tbl_gend, tbl_birt, tbl_age, tbl_aliv, tbl_deat, tbl_chil, tbl_spou])
 # y.add_row([bla,bla])
 print(x)
