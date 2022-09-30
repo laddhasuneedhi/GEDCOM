6fc21a52493d4aca70eb71ec7f88890d38e5399c
Hao Dian Li <hli144@stevens.edu>
2022-09-30 00:36:25 -0400

    more accurate calculation of age

--- sourcecode.py
+++ sourcecode.py
@@ -1,10 +1,12 @@
-#I pledge my honor that I have abided by the Stevens Honors System.
+# I pledge my honor that I have abided by the Stevens Honors System.
 # https://github.com/laddhasuneedhi/Project02.git
-#Hao Dian Li, Suneedhi Laddha, Ali El Sayed, Gigi Luna
+# Hao Dian Li, Suneedhi Laddha, Ali El Sayed, Gigi Luna
 
 import sys
 from prettytable import PrettyTable
-from datetime import date
+from datetime import date, datetime
+import calendar
+from datetime import datetime
 
 validtags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
 tag0 = ["INDI", "FAM", "HEAD", "TRLR", "NOTE"]
@@ -38,8 +40,10 @@ birfday = 0; deafday = 0; todays_date = date.today(); tblx_sarr = []; tblx_carr 
 name_list = []; id_list = []
 
 def _matchId(id):
+    
     gotmatch = 0
     fcopy = open(arg_1, 'r')
+
     for line in fcopy:
 
         matchToken = line.split() #list of the line
@@ -49,8 +53,13 @@ def _matchId(id):
         if (int(matchToken[0]) == 0) and (gotmatch == 1) and (matchToken[2] == "INDI" or "FAM"): return "N/A"
         if (int(matchToken[0]) == 0) and (matchToken[1] == id): gotmatch = 1
         if (int(matchToken[0]) == 1) and (matchToken[1] == "NAME") and (gotmatch == 1): gotmatch = 0; matchFullStr = ' '.join(str(i) for i in matchStrList); return matchFullStr; print (matchFullStr)
+    
     fcopy.close()
 
+def age(given_date, birthdate):
+
+    age = given_date.year - birthdate.year - ((given_date.month, given_date.day) < (birthdate.month, birthdate.day))
+    return age
 
 # """
 for line in f:
@@ -66,7 +75,7 @@ for line in f:
 
     tok1 = token[1] #second token: tags
 
-    if tok1 in validtags: #second token is a valid tag
+    if tok1 in validtags: #check if second token is a valid tag
         # if numtok < 3: #only a tag is present, no string        ex: 1 BIRT/MARR/DIV
         #     print("todo: " + tok1)
 
@@ -79,6 +88,7 @@ for line in f:
         fullStr = ' '.join(str(i) for i in strList)
 
         if (tok0 == 1) and (tok1 in tag1):
+
             if tok1 == "NAME": tblx_name = fullStr
             if tok1 == "DIV": divfday = 1
             if tok1 == "BIRT": birfday = 1
@@ -88,12 +98,10 @@ for line in f:
             if tok1 == "FAMC": tblx_carr.append(fullStr); tblx_chil = tblx_carr
             if tok1 == "FAMS": tblx_sarr.append(fullStr); tblx_spou = tblx_sarr
             if tok1 == "CHIL": tbly_carr.append(fullStr); tbly_chil = tbly_carr
-
             if tok1 == "WIFE":
                 tbly_wifi = fullStr
                 matchName = _matchId(tbly_wifi)
                 tbly_wifn = matchName
-
             if tok1 == "HUSB":
                 tbly_husi = fullStr
                 matchName = _matchId(tbly_husi)
@@ -107,7 +115,7 @@ for line in f:
                 if marfday == 1: tbly_marr = fullStr; marfday = 0
                 if divfday == 1: tbly_divo = fullStr; divfday = 0
     
-    else: #third token is a valid tag   ex: INDI or FAM
+    else: #check if third token is a valid tag   ex: INDI or FAM
 
         tok2 = token[2]
 
@@ -115,10 +123,18 @@ for line in f:
             if tok2 == "INDI":
                 if tblx_id != "N/A":
                     
-                    birth_split = tblx_birt.split(" ")
-                    death_split = tblx_deat.split(" ")
-                    if tblx_aliv == True: tblx_age = todays_date.year - int(birth_split[2])
-                    elif tblx_aliv == False: tblx_age = int(death_split[2]) - int(birth_split[2])
+                    # calculate accurate ages
+                    today = date.today()
+                    birth_split = tblx_birt.split()
+                    death_split = tblx_deat.split()
+                    # convert month name to number  ex: May -> 5
+                    abbr_to_num = {name: num for num, name in enumerate(calendar.month_abbr) if num}
+                    bmn_to_num = abbr_to_num[birth_split[1]]
+
+                    if tblx_aliv == True: tblx_age = age(today, date(int(birth_split[2]), bmn_to_num, int(birth_split[0])))
+                    elif tblx_aliv == False:
+                        dmn_to_num = abbr_to_num[death_split[1]]
+                        tblx_age = age(date(int(death_split[2]), dmn_to_num, int(death_split[0])), date(int(birth_split[2]), bmn_to_num, int(birth_split[0])))
 
                     x.add_row([tblx_id, tblx_name, tblx_gend, tblx_birt, tblx_age, tblx_aliv, tblx_deat, tblx_chil, tblx_spou])
                     tblx_id = "N/A"; tblx_name = "N/A"; tblx_gend = "N/A"; tblx_birt = "N/A"; tblx_age = "N/A"; tblx_aliv = "N/A"; tblx_deat = "N/A"; tblx_chil = "N/A"; tblx_spou = "N/A"; tblx_carr = []; tblx_sarr = []
@@ -142,10 +158,16 @@ for line in f:
         # else: 
         #     print("Invalid tag as 3rd token")
 
-birth_split = tblx_birt.split(" ")
-death_split = tblx_deat.split(" ")
-if tblx_aliv == True: tblx_age = todays_date.year - int(birth_split[2])
-elif tblx_aliv == False: tblx_age = int(death_split[2]) - int(birth_split[2])
+today = date.today()
+birth_split = tblx_birt.split()
+death_split = tblx_deat.split()
+abbr_to_num = {name: num for num, name in enumerate(calendar.month_abbr) if num}
+bmn_to_num = abbr_to_num[birth_split[1]]
+
+if tblx_aliv == True: tblx_age = age(today, date(int(birth_split[2]), bmn_to_num, int(birth_split[0])))
+elif tblx_aliv == False:
+    dmn_to_num = abbr_to_num[death_split[1]]
+    tblx_age = age(date(int(death_split[2]), dmn_to_num, int(death_split[0])), date(int(birth_split[2]), bmn_to_num, int(birth_split[0])))
 
 x.add_row([tblx_id, tblx_name, tblx_gend, tblx_birt, tblx_age, tblx_aliv, tblx_deat, tblx_chil, tblx_spou])
 y.add_row([tbly_id, tbly_marr, tbly_divo, tbly_husi, tbly_husn, tbly_wifi, tbly_wifn, tbly_chil])
