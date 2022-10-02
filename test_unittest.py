e28e829801b859e5272b617234995117fa401440
laddhasuneedhi <phantrash360@gmail.com>
2022-10-02 14:29:37 -0700

    chaged the first testcase

--- test_unittest.py
+++ test_unittest.py
@@ -4,7 +4,7 @@ from datetime import date
 
 class Test_Testsourcecode(unittest.TestCase):
     def test1_us11(self):
-        self.assertEqual(sourcecode._us11(["25", "JAN", "1930"],["10", "APR", "1940"],  "@I2@"), [['@I2@', '1961-12-16', '1960-03-25']])
+        self.assertEqual(sourcecode._us11(["25", "JAN", "1930"],["10", "APR", "1940"],  "@I2@"), [['@I2@' , '1940-04-10','1930-01-25']])
     def test2_us11(self):
         self.assertEqual(sourcecode._us11(["1", "DEC", "2009"],["30", "MAR", "2008"],  "@I2@"), [])
     def test3_us11(self):
